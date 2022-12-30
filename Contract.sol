659aa68f387394ec8f32cbd8ef6a84746b1d61bd
Leon Do <leondo123@gmail.com>
2022-12-30 01:30:46 -0500

    Update Contract.sol

--- Contract.sol
+++ Contract.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
 
-contract SemiERC1155 is ERC1155URIStorage {
+contract SemiCustodialERC1155 is ERC1155URIStorage {
     // Set default uri
     string private constant DEFAULT_URI = "https://";
 
