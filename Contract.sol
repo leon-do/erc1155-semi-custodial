fae9711f157fe789d16b846a91c495a6e5788dfd
Leon Do <leondo123@gmail.com>
2022-12-30 20:33:42 -0500

    Update Contract.sol

--- Contract.sol
+++ Contract.sol
@@ -3,30 +3,18 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
 
-contract CustodialERC1155 is ERC1155URIStorage {
+contract Dexoshi is ERC1155URIStorage {
     // Set default uri
-    string private constant DEFAULT_URI = "https://";
+    string private constant DEFAULT_URI = "@dexoshi";
 
     // Admin is allowed to call functions on behalf of player
-    address private constant ADMIN = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-
-    // Maps player custody. default is false aka player has no custody for better UX
-    mapping(address => bool) public playerCustody;
+    address private constant ADMIN = 0xdA064B1Cef52e19caFF22ae2Cc1A4e8873B8bAB0;
 
     constructor() ERC1155(DEFAULT_URI) {}
 
     /*
-    * Toggles player custody.
-    * @param _bool false to disable player custody. Server DOES    control cards. Player DOESN'T pay gas
-    * @param _bool true  to enable  player custody. Server DOESN'T control cards. Player DOES    pay gas
-    */
-    function setCustody(bool _bool) public {
-        playerCustody[msg.sender] = _bool;
-    }
-
-    /*
     * Only admin can mint
-    * @param _to address to mint to
+    * @param _to address to transfer to
     * @param _tokenId token identifier
     * @param _amount of tokens to mint
     * @param _tokenURI uri to metadata json object
@@ -47,8 +35,19 @@ contract CustodialERC1155 is ERC1155URIStorage {
     */
     function adminSafeTransferFrom(address _from, address _to, uint256 _tokenId, uint256 _amount) public {
         require(msg.sender == ADMIN, "Only admin can transfer");
-        require(playerCustody[_from] == false, "Only admin with custody can transfer");
         _safeTransferFrom(_from, _to, _tokenId, _amount, "");
+    }
+
+    /*
+    * Only admin with custody can batch transfer
+    * @params _from address. Admin cannot transfer if from address has custody
+    * @params _to address to transfer to
+    * @params _tokenIds list of tokens
+    * @params _amounts list of amounts
+    */
+    function adminSafeBatchTransferFrom(address _from, address _to, uint256[] memory _tokenIds, uint256[] memory _amounts) public {
+        require(msg.sender == ADMIN, "Only admin can burn");
+        _safeBatchTransferFrom(_from, _to, _tokenIds, _amounts, "");
     }
 
     /*
@@ -59,7 +58,6 @@ contract CustodialERC1155 is ERC1155URIStorage {
     */
     function adminBurn(address _from, uint256 _tokenId, uint256 _amount) public {
         require(msg.sender == ADMIN, "Only admin can burn");
-        require(playerCustody[_from] == false, "Only admin with custody can burn");
         _burn(_from, _tokenId, _amount);
     }
 
